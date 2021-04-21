8b20e46ac74ee781889d98c4df3f7c54084a556f
lime-de-rhyme <79634526+lime-de-rhyme@users.noreply.github.com>
2021-04-21 14:55:21 +0900

    Add files via upload
    first commit

---
+++ 774inc_schedule/all_livers.py
@@ -0,0 +1,77 @@
+livers_list = {
+    #Haneru Channel / 因幡はねる 【あにまーれ】
+      "UC0Owc36U9lOyi9Gx9Ic-4qg":
+      "https://yt3.ggpht.com/ytc/AAUvwngvgRbvQSxZHcosptXrl6PO3djyKHY7ZLIGbQHo=s176-c-k-c0x00ffffff-no-rj",
+    #Ichika Channel / 宗谷 いちか 【あにまーれ】
+      "UC2kyQhzGOB-JPgcQX9OMgEw":
+      "https://yt3.ggpht.com/ytc/AAUvwnj2XS4F6MS5aaKGDaQ4mcrPlW44lEN-p9oXqj9x=s176-c-k-c0x00ffffff-no-rj",
+    #Ran Channel / 日ノ隈らん 【あにまーれ】
+      "UCRvpMpzAXBRKJQuk-8-Sdvg":
+      "https://yt3.ggpht.com/ytc/AAUvwnit824wjmPA7AE7LDtG0EVjmutNl4-sICtStfTh=s176-c-k-c0x00ffffff-no-rj",
+    #Kuku Channel / 風見くく 【あにまーれ】
+      "UCXp7sNC0F_qkjickvlYkg-Q":
+      "https://yt3.ggpht.com/ytc/AAUvwngUM0DtcqRxbIHvfd8t3D-YEFLudJbN29dpdy44=s176-c-k-c0x00ffffff-no-rj",
+    #Izumi Channel / 柚原いづみ 【あにまーれ】
+      "UCW8WKciBixmaqaGqrlTITRQ":
+      "https://yt3.ggpht.com/ytc/AAUvwnhkL2lOLKPB4cqpPgqjHDV9AVypXtkNt10Eqm0=s176-c-k-c0x00ffffff-no-rj",
+    #Mimi Channel / 白宮みみ 【あにまーれ】
+      "UCtzCQnCT9E4o6U3mHHSHbQQ":
+      "https://yt3.ggpht.com/ytc/AAUvwnh5PNIZ9uxa95u863cHH2MYouiMqeau7SMPMgIM2g=s176-c-k-c0x00ffffff-no-rj",
+    #Natsumi Channel / 羽柴なつみ 【あにまーれ】
+      "UC_BlXOQe5OcRC7o0GX8kp8A":
+      "https://yt3.ggpht.com/ytc/AAUvwnhnPVaFf9fcrI0-QGkOQ-qSEz2jx0KI8VdkdzGc=s176-c-k-c0x00ffffff-no-rj",
+    #Hikari Channel / 飛良ひかり 【あにまーれ】
+      "UCFsWaTQ7kT76jNNGeGIKNSA":
+      "https://yt3.ggpht.com/ytc/AAUvwnggEiqlVN2rxQJAOdr_FDt5FxmAc_zqTKT-Jo1L=s176-c-k-c0x00ffffff-no-rj",
+    #Rui Channel / 瀬島るい 【あにまーれ】
+      "UC_WOBIopwUih0rytRnr_1Ag":
+      "https://yt3.ggpht.com/ytc/AAUvwniT9Kk84h82ycAnKFqBNqWSb6GYU9msUCuICnU1=s176-c-k-c0x00ffffff-no-rj",
+    #Tirol Channel /月野木ちろる 【あにまーれ】
+      "UCqskJ0nmw-_eweWfsKvbrzQ":
+      "https://yt3.ggpht.com/ytc/AAUvwnjuOZitdf-fShd8pKRuJphKOBul_WJ9ikhVJC0t=s176-c-k-c0x00ffffff-no-rj",
+    #Rukako Channel / 大浦るかこ 【あにまーれ】
+      "UC3xG1XWzAKt5dxSxktJvtxg":
+      "https://yt3.ggpht.com/ytc/AAUvwni5YbJevR91U1BH8ZnAht-KGKlfR9qQ0mq--Ms-=s176-c-k-c0x00ffffff-no-rj",
+    #Mia Channel / 湖南みあ 【あにまーれ】
+      "UC4PrHgUcAtOoj_LKmUL-uLQ":
+      "https://yt3.ggpht.com/ytc/AAUvwnhx0c80q3E3hw9sh-DaAcCdJzQwoj-RGacvGKCr=s176-c-k-c0x00ffffff-no-rj",
+    #Patra Channel / 周防パトラ 【ハニスト】
+      "UCeLzT-7b2PBcunJplmWtoDg":
+      "https://yt3.ggpht.com/ytc/AAUvwni3K3HvrlssJykNA7cE68zGcO4xZPF5k3XLE9sO=s176-c-k-c0x00ffffff-no-rj",
+    #Mico Channel / 堰代ミコ【ハニスト】
+      "UCDh2bWI5EDu7PavqwICkVpA":
+      "https://yt3.ggpht.com/ytc/AAUvwnj3utKMlGG9fe71Ti1JfPMohMvO_gT_oSCdGC51=s176-c-k-c0x00ffffff-no-rj",
+    #Mary Channel / 西園寺メアリ【ハニスト】
+      "UCwePpiw1ocZRSNSkpKvVISw":
+      "https://yt3.ggpht.com/ytc/AAUvwnhhEokdfq1Mo2x7_TmP2AU-v0nqHFchVBNWqay_=s176-c-k-c0x00ffffff-no-rj",
+    #Charlotte Channel / 島村シャルロット【ハニスト】
+      "UCYTz3uIgwVY3ZU-IQJS8r3A":
+      "https://yt3.ggpht.com/ytc/AAUvwnjfBbGgsin15WvNkf1f1KvMA956OOECN8FswkYU=s176-c-k-c0x00ffffff-no-rj",
+    #Met Channel / 小森めと 【ブイアパ】
+      "UCzUNASdzI4PV5SlqtYwAkKQ":
+      "https://yt3.ggpht.com/ytc/AAUvwngcfYNy2eRoiQds8qhnxWohjghC3WcxLBtOX5Kg=s176-c-k-c0x00ffffff-no-rj",
+    #Wat Channel / 不磨わっと 【ブイアパ】
+      "UCV4EoK6BVNl7wxuxpUvvSWA":
+      "https://yt3.ggpht.com/ytc/AAUvwnghel9DJYtFb6bSWzpybgFvZVXW5JVewnf7t3X-=s176-c-k-c0x00ffffff-no-rj",
+    #花奏 かのん / Kanade Kanon ch
+      "UCmqrvfLMws-GLGHQcB5dasg":
+      "https://yt3.ggpht.com/ytc/AAUvwngyYdZqbOy-sD-6uapvOvPFivU6Aizkkjkz0lWqbg=s176-c-k-c0x00ffffff-no-rj",
+    #Anko Channel / 季咲あんこ 【ブイアパ】
+      "UChXm-xAYPfygrbyLo2yCASQ":
+      "https://yt3.ggpht.com/ytc/AAUvwnguDljqOoJWknfaN1vN3F8NPPPBMydXFK-ljH05=s176-c-k-c0x00ffffff-no-rj",
+    #Uge Channel / 杏戸ゆげ 【ブイアパ】
+      "UC3EhsuKdEkI99TWZwZgWutg":
+      "https://yt3.ggpht.com/ytc/AAUvwnhldEYLmpc864v-XQDKTryZmS33CK_91Wf2Iwb1=s176-c-k-c0x00ffffff-no-rj",
+    #Anna Channel / 虎城アンナ 【シュガリリ】
+      "UCvPPBoTOor5gm8zSlE2tg4w":
+      "https://yt3.ggpht.com/ytc/AAUvwng3pwz9U-DRhU-oVEo42HFGbzOElFOMHZ532dlV=s176-c-k-c0x00ffffff-no-rj",
+    #Chris Channel / 獅子王クリス 【シュガリリ】
+      "UC--A2dwZW7-M2kID0N6_lfA":
+      "https://yt3.ggpht.com/ytc/AAUvwngzuEXJ6hxBH1uZtz_iJv1VRC8mMGlqEOXojp9f=s176-c-k-c0x00ffffff-no-rj",
+    #Rene Channel / 龍ヶ崎リン 【シュガリリ】
+      "UC2hc-00y-MSR6eYA4eQ4tjQ":
+      "https://yt3.ggpht.com/ytc/AAUvwnhPqY2LTUK6Eo6Fl1ZH_kL-Z9QhjDl9a46iwRH-=s176-c-k-c0x00ffffff-no-rj",
+    #奇想天外あにびっと!
+      "UC0xhrAce06OkQfHBqAfLQAQ":
+      "https://yt3.ggpht.com/ytc/AAUvwniyHQNKdGAQrIe2RGD5z0JI1v4aVAGxCoFizndW=s176-c-k-c0x00ffffff-no-rj",
+  };
---
+++ 774inc_schedule/main.py
@@ -0,0 +1,220 @@
+from apiclient.discovery import build
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
+
+import all_livers
+import constants
+
+
+# 初期化済みかを判定する
+if not firebase_admin._apps:
+    # 初期済みでない場合は初期化処理を行う
+    cred = credentials.Certificate("./inc-scheduler-firebase-adminsdk-xbkws-fef985c11e.json")
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+youtube = build('youtube', 'v3', developerKey=constants.YOUTUBE_API_KEY)
+scheduler = BlockingScheduler()
+
+    
+
+def creat_publishedAfter():
+    now = datetime.datetime.now() # 2019-02-04 21:04:15.412854
+    midnight = now - datetime.timedelta(days=1)
+    month = '0'+str(midnight.month)+'-' if midnight.month < 10 else str(midnight.month)+'-'
+    day = '0'+str(midnight.day) if midnight.day < 10 else str(midnight.day)
+    hour = '0'+str(midnight.hour) if midnight.hour < 10 else str(midnight.hour)
+    publishedAfter = str(midnight.year) + '-' + month + day + 'T' + hour + ':00:00Z'
+    print(publishedAfter)
+    return publishedAfter
+
+
+def get_search():
+    search_response = youtube.search().list(part= 'snippet',
+                                            eventType= 'none',
+                                            maxResults= '50',
+                                            publishedAfter= creat_publishedAfter(),
+                                            q= '774inc',
+                                            type= 'video').execute()
+    video_ids = []
+    channel_ids = []
+    for item in search_response['items']:
+        video_ids.append(item['id']['videoId'])
+        channel_ids.append(item['snippet']['channelId'])
+    return video_ids, channel_ids
+
+
+def get_search_upcoming():
+    search_response = youtube.search().list(part= 'snippet',
+                                            eventType= 'upcoming',
+                                            maxResults= '50',
+                                            q= '774inc',
+                                            type= 'video').execute()
+    video_ids = []
+    channel_ids = []
+    for item in search_response['items']:
+        video_ids.append(item['id']['videoId'])
+        channel_ids.append(item['snippet']['channelId'])
+    return video_ids, channel_ids
+
+
+def get_video(video_id):
+    video = youtube.videos().list(part = 'snippet,liveStreamingDetails',
+                                  id = video_id).execute()['items'][0]
+    return video
+
+
+def iso_format(scheduledStartTime):
+    dt = datetime.datetime.fromisoformat(scheduledStartTime[:-1])
+    return dt
+
+
+print('This job is main.')
+video_ids, channel_ids = get_search()
+upcoming_video_ids, upcoming_channel_ids = get_search_upcoming()
+video_ids += upcoming_video_ids
+channel_ids += upcoming_channel_ids
+
+
+for i in range(len(video_ids)):
+    #チャンネルアイコンの取得と774inc所属以外のチャンネルなら以降の処理をしない
+    try:
+        channel_icon = all_livers.livers_list[channel_ids[i]]
+    except KeyError as e:
+        print('catch KeyError:', e)
+        continue
+    
+    video = get_video(video_ids[i])
+    dt = iso_format(video['liveStreamingDetails']['scheduledStartTime'])
+    
+    print('scheduledStartTime', dt)
+    print('title', video['snippet']['title'])
+    print('videoStatus', video['snippet']['liveBroadcastContent'])
+    print('------------------------------------------------------------------')
+    print('')
+    #firestoreにデータを格納
+    doc_ref = db.collection('videos').document(video_ids[i])
+    doc = doc_ref.get()
+    if doc.exists:
+        doc_ref.update({
+            'iconUrl': channel_icon,
+            'channelTitle': video['snippet']['channelTitle'],
+            'scheduledStartTime': dt,
+            'thumbnailUrl': video['snippet']['thumbnails']['medium']['url'],
+            'title': video['snippet']['title'],
+            'videoStatus': video['snippet']['liveBroadcastContent'],
+            'videoUrl': 'https://www.youtube.com/watch?v=' + video_ids[i],
+            'channelUrl': 'https://www.youtube.com/channel/' + video['snippet']['channelId']
+            })
+    else:
+        doc_ref.set({
+            'iconUrl': channel_icon,
+            'channelTitle': video['snippet']['channelTitle'],
+            'scheduledStartTime': dt,
+            'thumbnailUrl': video['snippet']['thumbnails']['medium']['url'],
+            'title': video['snippet']['title'],
+            'videoStatus': video['snippet']['liveBroadcastContent'],
+            'videoUrl': 'https://www.youtube.com/watch?v=' + video_ids[i],
+            'channelUrl': 'https://www.youtube.com/channel/' + video['snippet']['channelId']
+            })
+
+
+@scheduler.scheduled_job('interval', minutes=5)
+def live_status():
+    print('excute live_status')
+    for video_doc in db.collection('videos').where('videoStatus', '==', 'live').get():
+        try:
+            video = get_video(video_doc.id)
+        except IndexError as e:
+            print(e)
+            print('アーカイブ消された')
+            #firestoreからdocumentを削除
+            db.collection('videos').document(video_doc.id).delete()
+            continue
+        #余分にfirestoreの書き込みを行いたくないので更新があった場合だけupdateするようにしたい
+        dt = iso_format(video['liveStreamingDetails']['scheduledStartTime'])
+        thumbnailUrl = video['snippet']['thumbnails']['medium']['url']
+        title = video['snippet']['title']
+        video_status = video['snippet']['liveBroadcastContent']
+        doc_ref = db.collection('videos').document(video_doc.id)
+        dt_fs = str(doc_ref.get(field_paths={'scheduledStartTime'}).to_dict().get('scheduledStartTime'))
+        
+        if (str(dt) == dt_fs[:19] and
+            thumbnailUrl == doc_ref.get(field_paths={'thumbnailUrl'}).to_dict().get('thumbnailUrl') and
+            title == doc_ref.get(field_paths={'title'}).to_dict().get('title') and
+            video_status == doc_ref.get(field_paths={'videoStatus'}).to_dict().get('videoStatus')):
+            continue
+        else:
+            #firestoreにデータを格納
+            doc_ref.update({
+                'scheduledStartTime': dt,
+                'thumbnailUrl': thumbnailUrl,
+                'title': title,
+                'videoStatus': video_status,
+                })
+        
+
+def main_processing():
+    print('This job is main.')
+    video_ids, channel_ids = get_search()
+    upcoming_video_ids, upcoming_channel_ids = get_search_upcoming()
+    video_ids += upcoming_video_ids
+    channel_ids += upcoming_channel_ids
+    
+    
+    for i in range(len(video_ids)):
+        #チャンネルアイコンの取得と774inc所属以外のチャンネルなら以降の処理をしない
+        try:
+            channel_icon = all_livers.livers_list[channel_ids[i]]
+        except KeyError as e:
+            print('catch KeyError:', e)
+            continue
+        
+        video = get_video(video_ids[i])
+        dt = iso_format(video['liveStreamingDetails']['scheduledStartTime'])
+        
+        print('iconUrl', channel_icon)
+        print('channelTitle', video['snippet']['channelTitle'])
+        print('scheduledStartTime', dt)
+        print('thumbnailUrl', video['snippet']['thumbnails']['medium']['url'])
+        print('title', video['snippet']['title'])
+        print('videoStatus', video['snippet']['liveBroadcastContent'])
+        print('videoUrl', 'https://www.youtube.com/watch?v=' + video_ids[i])
+        print('channelUrl', 'https://www.youtube.com/channel/' + video['snippet']['channelId'])
+        print('------------------------------------------------------------------')
+        print('')
+        #firestoreにデータを格納
+        doc_ref = db.collection('videos').document(video_ids[i])
+        doc = doc_ref.get()
+        if doc.exists:
+            doc_ref.update({
+                'iconUrl': channel_icon,
+                'channelTitle': video['snippet']['channelTitle'],
+                'scheduledStartTime': dt,
+                'thumbnailUrl': video['snippet']['thumbnails']['medium']['url'],
+                'title': video['snippet']['title'],
+                'videoStatus': video['snippet']['liveBroadcastContent'],
+                'videoUrl': 'https://www.youtube.com/watch?v=' + video_ids[i],
+                'channelUrl': 'https://www.youtube.com/channel/' + video['snippet']['channelId']
+                })
+        else:
+            doc_ref.set({
+                'iconUrl': channel_icon,
+                'channelTitle': video['snippet']['channelTitle'],
+                'scheduledStartTime': dt,
+                'thumbnailUrl': video['snippet']['thumbnails']['medium']['url'],
+                'title': video['snippet']['title'],
+                'videoStatus': video['snippet']['liveBroadcastContent'],
+                'videoUrl': 'https://www.youtube.com/watch?v=' + video_ids[i],
+                'channelUrl': 'https://www.youtube.com/channel/' + video['snippet']['channelId']
+                })
+scheduler.add_job(main_processing, 'cron', minute=1)
+    
+    
+scheduler.start()  
+
+
+    
